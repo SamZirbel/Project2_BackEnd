a963b1b154d0788806dd1bd9fa269ba1619e7a2a
umi729 <umer.zahid@revature.net>
2021-11-06 18:44:25 -0700

    pushing by umer

--- Project2/src/main/java/com/revature/models/Favorite.java
+++ Project2/src/main/java/com/revature/models/Favorite.java
@@ -1,11 +1,12 @@
 package com.revature.models;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-
+@Entity
 public class Favorite {
 
 	@Id
