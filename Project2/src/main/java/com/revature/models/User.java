a3e4881b114f7f0f618eb82a546580733bc83dbd
umi729 <umer.zahid@revature.net>
2021-11-06 20:04:55 -0700

    After testing 2 methods

--- Project2/src/main/java/com/revature/models/User.java
+++ Project2/src/main/java/com/revature/models/User.java
@@ -2,13 +2,13 @@
 package com.revature.models;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="tbl_user")
 public class User {
 
     @Id
--- Project2/src/test/java/com/revature/services/UserServiceTest.java
+++ Project2/src/test/java/com/revature/services/UserServiceTest.java
@@ -1,37 +1,37 @@
 package com.revature.services;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import com.revature.models.User;
 import com.revature.repos.UserRepo;
-
+@SpringBootTest
 class UserServiceTest {
 	
+
 	@Autowired
-	public UserRepo userrepo;
-	
+	private UserRepo userRepo;
 
 	@Test
 	void testFindByUsername() {
 
-		Optional<List<User>> olist = userrepo.findByUsername("Sample");
-		
-		assertTrue(olist.isPresent());
+		Optional<List<User>> olist = userRepo.findByUsername("Sample");
+		assertTrue(!(olist.isEmpty()));
 		
 	}
 
 	@Test
 	void testFindAll() {
-		//fail("Not yet implemented");
-	}
+	List<User> olist = userRepo.findAll();
+		System.out.println(olist);
+		
+		}
 
 	@Test
 	void testFindById() {
