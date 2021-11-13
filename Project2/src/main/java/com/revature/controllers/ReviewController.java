ea6fbb53439ae4e1e9724fab9b3818527fe31068
Benjamin-Linthicum <benlinth@udel.edu>
2021-11-13 10:04:26 -0500

    Can now find reviews by movie

--- Project2/src/main/java/com/revature/controllers/ReviewController.java
+++ Project2/src/main/java/com/revature/controllers/ReviewController.java
@@ -1,11 +1,17 @@
 package com.revature.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.services.ReviewService;
+import com.revature.models.Movie;
+import com.revature.models.Review;
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -21,6 +27,16 @@ public class ReviewController {
 		super ();
 		this.reviewservice = reviewservice;
 		
+	}
+
+	@GetMapping("/reviewsByMovie/{movie}")
+	public ResponseEntity<List<Review>> fetchReviewsByMovie(@PathVariable("movie") Movie movie){
+		List<Review> list = reviewservice.findByMovie(movie);
+		
+		if(list.isEmpty()){
+			return ResponseEntity.noContent().build();
+		}
+		return ResponseEntity.ok(list);
 	}
 
 	/* Note For Writing Paths:
